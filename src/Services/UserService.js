c16f926ba1a0cba0d4c674bd83ae1cb66e7e58f6
karine mullier <karine_mullier@hotmail.com>
2024-12-13 10:11:03 +0100

    ajout login avec token

--- src/Services/UserService.js
+++ src/Services/UserService.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 function createUser(user){
-    return axios.post("http://127.0.0.1:3000/campingpong/createUser", user)
+    return axios.post("http://127.0.0.1:3000/campingpong/createUser", user);
 }
 
-function loginUser(user){
-    return axios.post("http://127.0.0.1:3000/campingpong/loginUser", user)
+function loginUser(login){
+    return axios.post("http://127.0.0.1:3000/campingpong/loginUser", login);
 }
 
 export default {createUser, loginUser}
